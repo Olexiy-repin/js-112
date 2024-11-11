d49fca71b227fc016757956c84170021744293be
Olexiy-repin <repinlyoha@gmail.com>
2024-11-11 21:30:46 +0200

    classwork

---
+++ js/01-arrays.js
@@ -0,0 +1,29 @@
+/*
+ * Масиви
+ *
+ * - Створення масиву
+ * - Доступ до елементів
+ * - Перевизначення значення елемента
+ * - Довжина масиву
+ * - Індекс останнього елемента
+ */
+
+// const courses = ['HTML', 'CSS', 'JS'];
+
+// console.log(courses);
+
+// console.log(courses[0]);
+// console.log(courses[1]);
+// console.log(courses[2]);
+
+// courses[0] = 'React';
+
+// console.log(courses);
+
+// const firstElement = courses[0];
+
+// console.log(firstElement);
+
+// const lastElement = courses[courses.length - 1];
+
+// console.log(lastElement);
---
+++ js/03-methods.js
@@ -0,0 +1,65 @@
+/*
+ * Методи масиву
+ *
+ * - join
+ * - split (Метод рядка)
+ * - slice
+ * - concat
+ * - indexOf
+ * - push/pop
+ */
+
+/*
+? -------------------------
+*/
+
+// const allUsers = ['Elva Harper', 'Lily Wilkins', 'Derek Medina', 'Derek Medina'];
+// const allUsersStr = allUsers.join(', ');
+
+// console.log(allUsers);
+// console.log(allUsersStr);
+
+/*
+? -------------------------
+*/
+
+// const phoneNumbersStr = '+380961111111,+380961111234,+380961111876';
+// const phoneNumbersArr = phoneNumbersStr.split(',');
+
+// console.log(phoneNumbersStr);
+// console.log(phoneNumbersArr);
+
+/*
+? -------------------------
+*/
+
+// const allUsers = ['Elva Harper', 'Lily Wilkins', 'Derek Medina', 'Charlie Boyd', 'Jon Rowe'];
+// const allUsersCopy = allUsers.slice(2, 4);
+
+// console.log(allUsers);
+// console.log(allUsersCopy);
+
+// allUsersCopy[0] = 'Noah Brady';
+
+// console.log(allUsers);
+// console.log(allUsersCopy);
+
+/*
+? -------------------------
+*/
+
+// const oldCourses = ['HTML', 'CSS', 'JavaScript'];
+// const newCourses = ['React', 'NodeJS'];
+// const allCoruses = newCourses.concat(oldCourses);
+
+// console.log(allCoruses);
+
+// allCoruses.push('Angular');
+// allCoruses.pop();
+// allCoruses.pop();
+
+// allCoruses.unshift('Angular');
+// allCoruses.shift();
+// allCoruses.shift();
+
+// console.log(allCoruses);
