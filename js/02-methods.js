7ecd3a82b570974ac3e8c7692ad52eff866430f1
Olexiy-repin <repinlyoha@gmail.com>
2024-11-20 17:58:20 +0200

    preparation for classwork

---
+++ js/02-methods.js
@@ -0,0 +1,12 @@
+/*
+ * Об'єкти
+ *
+ * - Методи об'єкта
+ * - Доступ до властивостей об'єкта через this
+ */
+
+const playlist = {
+  name: 'My amazing playlist',
+  rating: 5,
+  tracks: ['track-1', 'track-2', 'track-3'],
+};
